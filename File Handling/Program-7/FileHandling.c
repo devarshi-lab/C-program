85853708eb9278cf4d8b06a22ac81e01f989ef63
Devaa Pimpale <63437204+devarshi-lab@users.noreply.github.com>
2020-09-24 21:58:40 +0530

    Add files via upload

---
+++ File Handling/Program-7/FileHandling.c
@@ -0,0 +1,49 @@
+//Accept 2 file name from user and copy the contents of one file into another.
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<string.h>
+#include<io.h>
+
+
+void CopyFileData(char *name1,char *name2)
+{
+	int fd1 = 0,fd2 = 0,i=0;
+	char data[100] = {'\0'};
+	int ret = 0;
+
+	fd1 = open(name1,O_RDONLY,0777);
+	fd2 = open(name2,O_RDWR | O_CREAT,0777);
+
+	if(fd1 == -1 || fd2 == -1)
+	{
+		printf("ERROR : Unable to open file or create File.\n");
+		return;
+	}
+
+	while((ret = read(fd1,data,100)) != 0)
+	{
+		write(fd2,data,ret);
+	}
+
+	printf("Data Successfully written in file '%s' from '%s\n",name2,name1);
+	close(fd1);
+	close(fd2);
+}
+
+int main()
+{
+	char fname1[50] = {'\0'};
+	char fname2[50] = {'\0'};
+
+	printf("Enter the file name which u want to copy : \t");
+	scanf("%s",fname1);
+
+	printf("Enter the file name where u want to copy data : \t");
+	scanf(" %s",fname2);
+
+	CopyFileData(fname1,fname2);
+
+	return 0;
+}
